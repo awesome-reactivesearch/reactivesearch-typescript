bdc728ca77287562837e5a374ef42f5f53f97377
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-19 00:45:19 +0530

    Add typescript example

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,67 @@
+// @ts-ignore TODO: remove after TS release
+import { ReactiveBase, ResultList, SelectedFilters, TextField, } from "@appbaseio/reactivesearch";
+
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+class App extends React.Component<any, any> {
+	public render() {
+		return (
+			<ReactiveBase
+				app="good-books-ds"
+				credentials="nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d"
+			>
+				<div className="row">
+					<div className="col">
+						<TextField
+							dataField="original_title.search"
+							componentId="BookSensor"
+						/>
+					</div>
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+					<div className="col">
+						<SelectedFilters />
+						<ResultList
+							componentId="SearchResult"
+							dataField="original_title"
+							from={0}
+							size={3}
+							onData={this.booksList}
+							className="result-list-container"
+							pagination={true}
+							react={{
+								and: 'BookSensor',
+							}}
+						/>
+					</div>
+				</div>
+			</ReactiveBase>
+		);
+	}
+
+	public booksList(data: any) {
+		return {
+			description: (
+				<div className="flex column justify-space-between">
+					<div>
+						<div>by <span className="authors-list">{data.authors}</span></div>
+						<div className="ratings-list flex align-center">
+							<span className="stars">
+								{
+									Array(data.average_rating_rounded).fill('x')
+										.map((item, index) => <i className="fas fa-star" key={index} />) // eslint-disable-line
+								}
+							</span>
+							<span className="avg-rating">({data.average_rating} avg)</span>
+						</div>
+					</div>
+					<span className="pub-year">Pub {data.original_publication_year}</span>
+				</div>
+      ),
+      image: data.image,
+      title: <div className="book-title" dangerouslySetInnerHTML={{ __html: data.original_title }} />,
+		};
+	}
 }
 
 export default App;
